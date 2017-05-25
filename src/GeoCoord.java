a05239795c2c0a2dcae25d0bdeacdce5861ab94d
ede1998 <ede1998@arcor.de>
2017-05-25 17:19:10 +0200

    Initial commit

---
+++ src/GeoCoord.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Erik on 25.05.2017.
+ */
+public class GeoCoord {
+    public double lambda;
+    public double beta;
+}
---
+++ src/Satellite.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Erik on 25.05.2017.
+ */
+public class Satellite {
+    protected GeoCoord position;
+}
