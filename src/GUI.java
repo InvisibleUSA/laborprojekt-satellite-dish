4a4bd31d01ed850ba0125edcb31e07dac02c2527
InvisibleUSA <lukas.niedergriese1@gmail.com>
2017-06-19 18:08:00 +0200

    main.java created

---
+++ src/GUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+/**
+ * Created by ln on 02.06.17.
+ */
+public class GUI {
+    private JPanel panel1;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /**
  * Created by ln on 25.05.17.
  */
@@ -8,11 +10,12 @@ public class Main {
 
     public static void main(String [] args){
         System.out.println("k ist: " + k);
-        Satellite panamsat = new Satellite(Converter.degToRad(45));
-        SatelliteDish dish = new SatelliteDish(new GeoCoord(Converter.degToRad(-9.2), Converter.degToRad(-48.8)));
+        Satellite panamsat = new Satellite(Converter.degToRad(Double.parseDouble(JOptionPane.showInputDialog("Satpos: ", -19.2))));
+        SatelliteDish dish = new SatelliteDish(new GeoCoord(Converter.degToRad(Double.parseDouble(JOptionPane.showInputDialog("Länge: ", 9.2))), Converter.degToRad(Double.parseDouble(JOptionPane.showInputDialog("Breite: ", 48.8)))));
         double delta = dish.getDelta(panamsat);
         double alpha = dish.getAlpha(panamsat);
         double psi = dish.getPsi(panamsat);
+        JOptionPane.showMessageDialog(null, "Delta is: " + Converter.radToDeg(delta) + "\n" + "Alpha is: " + Converter.radToDeg(alpha) + "\n" + "Psi is: " + Converter.radToDeg(psi));
         System.out.println("Delta is: " + Converter.radToDeg(delta));
         System.out.println("Alpha is: " + Converter.radToDeg(alpha));
         System.out.println("Psi is: " + Converter.radToDeg(psi));
